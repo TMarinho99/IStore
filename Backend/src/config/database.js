8b9badcfbc744f1e80556812a82590a0c77e00ae
Thiago Marinho <thiagomarinhobezerra@gmail.com>
2020-04-27 08:55:10 -0300

    Initial Commit

---
+++ Backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '25thiago99',
+    database: 'istore',
+    define: {
+        timestamps: true,
+        underscores: true,
+        underscoresAll: true,
+    },
+};
